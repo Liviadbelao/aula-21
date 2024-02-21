3ca851dd1eedfc43fdaad60f07e2e90201cd00a2
Livia Belao <livia-belao@aluno.senai.br>
2024-02-21 09:22:27 -0300

    refactor: configuração inicial.

--- App.jsx
+++ App.jsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>oi!</Text>
-      <StatusBar style="auto" />
+	return (
+		<View style={styles.container}>
+      <Text>Hello, World!</Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
-  );
+	);
 }
 
 const styles = StyleSheet.create({
